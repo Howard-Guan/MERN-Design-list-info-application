f1fac5480f84610a2a61708a8c36286c1ec3e4c6
Howard Guan <gzhljh@gmail.com>
2022-01-10 11:26:47 -0700

    Fix Grid attribute warnning

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
         </AppBar>
         <Grow in>
           <Container>
-            <Grid container justify="space-between" alignItem="stretch">
+            <Grid container justifyContent="space-between" alignitem="stretch">
               <Grid item xs={12} sm={7}>
                 <AppBar className={classes.appBar} position="static" color="inherit">
                   <Student />
